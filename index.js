e628e8b9463dee8fc88e748c707e46faea7a8fb5
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-01-13 15:03:06 -0500

    refactor to use fs.readFileSync

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 var bencode = require('bencode');
 var fs = require('fs');
 
-var info;
-var announceList;
-var tracker;
-
-
 function decode(fileName) {
-  fs.readFile(fileName, function(err, data) {
-    if (err) {
-      console.log('Error reading file ' + fileName);
-    }
-    var information = bencode.decode(data, 'utf8');
-    announceList = information['announce-list'];
-    info = information.info;
-    console.log(findTracker(announceList));
-  });
+  var information = bencode.decode(fs.readFileSync(fileName), 'utf8');
+  return {
+    announceList: information['announce-list'],
+    info: information.info
+  };
 }
 
 // return first http tracker
 function findTracker(list) {
   for (var i = 0; i < list.length; i++) {
-    var tracker = list[i];
-    if (tracker[0].search(/^http/) === 0) {
-      return tracker[0];
+    var item = list[i];
+    if (item[0].search(/^http/) === 0) {
+      return item[0];
     }
   }
 }
 
-decode('./test.torrent');
+console.log(decode('./test.torrent'));
